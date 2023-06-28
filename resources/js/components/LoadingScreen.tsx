03f8bfbefb680b234b2fce9c0762d5ec3394fb52
Kayode Matthew <matthewkayode116@gmail.com>
2023-06-28 17:55:21 +0100

    changed spinner color

--- resources/js/components/LoadingScreen.tsx
+++ resources/js/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import { Oval } from "react-loader-spinner";
 export default function LoadingScreen() {
     return (
         <div className="absolute h-screen w-screen top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-white">
-            <Oval color="white" />
+            <Oval color="#0052CC" secondaryColor="gray" />
         </div>
     );
 }
